3eaaa170e93b45bf5f3b7112d9a53ef745bf779c
Ali Arshad <ali015421@gmail.com>
2026-07-15 16:20:42 +0500

    Learnt variable assignment and naming conventions

---
+++ lessons/01-variables/lesson.py
@@ -0,0 +1,19 @@
+company_name = "AG Inc."
+number_of_employees = 50
+monthly_salary = 75000
+annual_bonus = 5000
+sales_revenue = 20000000
+cost_of_goods_sold = 1200000
+net_profit = sales_revenue - cost_of_goods_sold - (monthly_salary * 12) - annual_bonus
+tax_rate = 0.076
+tax_amount = net_profit * tax_rate
+
+print("Company name:", company_name)
+print("Number of employees is:", number_of_employees)
+print("Monthly salary is:", monthly_salary)
+print("Annual bonus is:", annual_bonus)
+print("Sales revenue is:", sales_revenue)
+print("Cost of goods sold is:", cost_of_goods_sold)
+print("Net profit is:", net_profit)
+print("Tax rate is:", tax_rate)
+print("Tax amount is:", tax_amount)
---
+++ lessons/01-variables/variable-names.py
@@ -0,0 +1,8 @@
+company_name = "AG Inc."
+number_of_employees = 50
+monthly_salary = 75000
+annual_bonus = 5000
+sales_revenue = 20000000
+cost_of_goods_sold = 1200000
+net_profit = sales_revenue - cost_of_goods_sold - (monthly_salary * 12) - annual_bonus
+tax_rate = 0.076
