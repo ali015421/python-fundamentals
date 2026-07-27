d655a97a385a1b5a25f4f3904031b1a77dbdecd2
Ali Arshad <ali015421@gmail.com>
2026-07-27 14:06:09 +0500

    learnt expressions and operators

--- lessons/03-expressions-operators/lesson.py
+++ lessons/03-expressions-operators/lesson.py
@@ -6,5 +6,26 @@ print(expenses == budget)
 income = 90000
 credit_score = 720
 
-income > 50000 and credit_score > 700
 
+income = 90000
+credit_score = 650
+
+
+print(income > 50000 and credit_score > 700)
+print(income > 50000 or credit_score > 700)
+
+balance = 5000
+balance += 1000
+print(balance)
+
+balance = 5000
+balance -= 1000
+print(balance)
+
+balance = 5000
+balance *= 10
+print(balance)
+
+balance = 5000
+balance /= 10
+print(balance)
